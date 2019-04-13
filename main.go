5ed54758d850c1dcaf0c998dc7f7692c4731564e
Dwi <ruckuus@gmail.com>
2019-04-13 18:23:10 +0800

    Default mux, simplest form.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "This is a homepage")
+	} else if r.URL.Path == "/hello" {
+		fmt.Fprintf(w, "Please contact: ruckuus@gmail.com")
+	} else {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "Under construction")
+	}
+}
+
+func main()  {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+}
