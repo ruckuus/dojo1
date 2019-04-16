0e23e0b2b3ab69eb685f6ffe387ebf2fca5f28a2
Dwi <ruckuus@gmail.com>
2019-04-16 13:51:37 +0800

    Add FAQ page and signup page

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"net/http"
 )
 
-var homeView *views.View
-var contactView *views.View
-var errorView *views.View
+var (
+	homeView *views.View
+	contactView *views.View
+	errorView *views.View
+	faqView *views.View
+	signupView *views.View
+)
 
 func home(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "text/html")
@@ -35,11 +39,18 @@ func CustomNotFound(w http.ResponseWriter, r *http.Request)  {
 	must(errorView.Render(w, errorData))
 }
 
+func faq(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	must(faqView.Render(w, nil))
+}
+
 func main()  {
 
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	errorView = views.NewView("bootstrap", "views/error.gohtml")
+	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 
@@ -49,8 +60,15 @@ func main()  {
 
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 
 	http.ListenAndServe(":3000", r)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
 }
 
 func must(err error)  {
