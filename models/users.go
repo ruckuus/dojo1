5cc2fef709184898ca5c20dda71b1d0b5fb8cc32
Dwi <ruckuus@gmail.com>
2019-04-20 16:28:34 +0800

    Remember token normalization

--- models/users.go
+++ models/users.go
@@ -238,16 +238,10 @@ func (us *userService) Authenticate(email, password string) (*User, error) {
 
 // Create will perform user validation before calling user creation function
 func (uv *userValidator) Create(user *User) error {
-	if user.Remember == "" {
-		token, err := rand.RememberToken()
-		if err != nil {
-			return err
-		}
-		user.Remember = token
-	}
 
 	err := runUserValidationFunctions(user,
 		uv.bcryptPassword,
+		uv.setRememberIfUnset,
 		uv.hmacRemember)
 	if err != nil {
 		return err
@@ -307,6 +301,20 @@ func (uv *userValidator) hmacRemember(user *User) error {
 	}
 
 	user.RememberHash = uv.hmac.Hash(user.Remember)
+	return nil
+}
+
+func (uv *userValidator) setRememberIfUnset(user *User) error {
+	if user.Remember != "" {
+		return nil
+	}
+
+	token, err := rand.RememberToken()
+	if err != nil {
+		return err
+	}
+
+	user.Remember = token
 	return nil
 }
 
