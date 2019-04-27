b237ca369afb85055073db805f80036c0a967df3
Dwi <ruckuus@gmail.com>
2019-04-27 11:10:14 +0800

    Create new gallery.

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,20 +1,52 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/ruckuus/dojo1/models"
 	"github.com/ruckuus/dojo1/views"
 	"net/http"
 )
 
 type Galleries struct {
 	NewView *views.View
+	gs      models.GalleryService
 }
 
-func NewGalleries() *Galleries {
+type GalleryForm struct {
+	Title string `schema:"title"`
+}
+
+func NewGalleries(services models.GalleryService) *Galleries {
 	return &Galleries{
 		NewView: views.NewView("bootstrap", "galleries/new"),
+		gs:      services,
 	}
 }
 
-func (g *Galleries) New(w http.ResponseWriter, r *http.Request)  {
+func (g *Galleries) New(w http.ResponseWriter, r *http.Request) {
 	g.NewView.Render(w, nil)
+}
+
+func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
+	var form GalleryForm
+
+	if err := parseForm(r, &form); err != nil {
+		vd.SetAlert(err)
+		g.NewView.Render(w, vd)
+		return
+	}
+
+	gallery := models.Gallery{
+		Title: form.Title,
+	}
+
+	if err := g.gs.Create(&gallery); err != nil {
+		vd.SetAlert(err)
+		g.NewView.Render(w, vd)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully created")
+
 }
