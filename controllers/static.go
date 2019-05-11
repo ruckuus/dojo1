e32d1eddd8a428c62f0499aead513897979c45ec
Dwi <ruckuus@gmail.com>
2019-05-11 15:23:34 +0800

    Tiny cosmetic.

--- controllers/static.go
+++ controllers/static.go
@@ -3,14 +3,14 @@ package controllers
 import "github.com/ruckuus/dojo1/views"
 
 type Static struct {
-	Home        *views.View
+	Home    *views.View
 	Contact *views.View
 	FAQ     *views.View
 }
 
-func NewStatic() *Static  {
+func NewStatic() *Static {
 	return &Static{
-		Home:        views.NewView("bootstrap", "static/home"),
+		Home:    views.NewView("bootstrap", "static/home"),
 		Contact: views.NewView("bootstrap", "static/contact"),
 		FAQ:     views.NewView("bootstrap", "static/faq"),
 	}
