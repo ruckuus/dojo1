9750678b0579c3799250e69edb33a8e2a322cfcc
Dwi <ruckuus@gmail.com>
2019-05-13 10:12:03 +0800

    Parse URL parameter.

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/gorilla/schema"
 	"net/http"
+	"net/url"
 )
 
 func parseForm(r *http.Request, dst interface{}) error {
@@ -11,15 +12,29 @@ func parseForm(r *http.Request, dst interface{}) error {
 		return err
 	}
 
+	return parseValues(r.PostForm, dst)
+
+}
+
+func parseValues(values url.Values, dst interface{}) error {
+
 	decoder := schema.NewDecoder()
+
 	// This is to tell gorilla/schema to ignore unknown key in the request
-	//
 	decoder.IgnoreUnknownKeys(true)
-	err := decoder.Decode(dst, r.PostForm)
+	err := decoder.Decode(dst, values)
 
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func parseURLParams(r *http.Request, dst interface{}) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+
+	return parseValues(r.Form, dst)
 }
--- controllers/users.go
+++ controllers/users.go
@@ -39,7 +39,9 @@ func NewUsers(us models.UserService) *Users {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, r, nil)
+	var form SignupForm
+	parseURLParams(r, &form)
+	u.NewView.Render(w, r, form)
 
 }
 
